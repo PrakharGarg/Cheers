0ebc627ed5fc470da4a8d5958a2afb14b06c39ce
PrakharGarg <prakhargarg@utexas.edu>
2017-02-15 16:24:56 -0600

    typo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -111,7 +111,7 @@ class HomeController < ApplicationController
     response = open('http://api.brewerydb.com/v2/styles?key=4bcd6a2eb064474ae82b23333279b0f3&format=json').read
     json = JSON.parse(response)
     json["data"].each do |style|
-      Style.create(@name: style["@name"], description: style["description"], abvMax: style["abvMax"])
+      Style.create(name: style["@name"], description: style["description"])
     end
   end
   
